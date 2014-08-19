d78a4b49ddeadb380c6400b3ce8449e3392d4f5d
Eric <erjohnson.pdx@gmail.com>
2014-08-19 05:53:05 +0000

    Add save, .all, and == methods to theater

--- lib/theater.rb
+++ lib/theater.rb
@@ -7,4 +7,27 @@ class Theater
     @knight_id = attributes[:knight_id]
     @campaign_id = attributes[:campaign_id]
   end
+  
+  def self.all
+    theaters = []
+    results = DB.exec("SELECT * FROM theaters;")
+    results.each do |result|
+      attributes = {
+        :id => result['id'].to_i,
+        :knight_id => result['knight_id'].to_i,
+        :campaign_id => result['campaign_id'].to_i
+      }
+      theaters << Theater.new(attributes)
+    end
+    theaters
+  end
+  
+  def save
+    result = DB.exec("INSERT INTO theaters (knight_id, campaign_id) VALUES (#{knight_id}, #{campaign_id}) RETURNING id;")
+    @id = result.first['id'].to_i
+  end
+  
+  def == arg
+    self.knight_id == arg.knight_id && self.campaign_id == arg.campaign_id && self.id == arg.id
+  end
 end
--- spec/theater_spec.rb
+++ spec/theater_spec.rb
@@ -15,4 +15,18 @@ describe 'Theater' do
     new_theater = Theater.new({:knight_id => 1, :campaign_id => 3})
     expect(new_theater.campaign_id).to eq 3
   end
+  
+  describe '.all' do
+    it 'starts as an empty array' do
+      expect(Theater.all).to eq []
+    end
+    
+    it 'returns all knights' do
+      new_theater1 = Theater.new({:knight_id => 1, :campaign_id => 3})
+      new_theater2 = Theater.new({:knight_id => 5, :campaign_id => 3})
+      new_theater1.save
+      new_theater2.save
+      expect(Theater.all).to eq [new_theater1, new_theater2]
+    end
+  end
 end
