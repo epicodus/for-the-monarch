f54ac1aaa3fc65f9f968d051d9b5154c2a29057e
Eric Johnson <student@epicodus.com>
2014-08-19 10:51:26 -0700

    Add check for knight campaign join

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,11 +1,11 @@
 class Knight
   attr_reader :id, :name
-  
+
   def initialize attributes
     @id = attributes[:id]
     @name = attributes[:name]
   end
-  
+
   def self.all
     knights = []
     results = DB.exec("SELECT * FROM knights;")
@@ -18,7 +18,7 @@ class Knight
     end
     knights
   end
-  
+
   def self.find id
     knight = []
     results = DB.exec("SELECT * FROM knights WHERE id = #{id};")
@@ -31,16 +31,33 @@ class Knight
     end
     knight[0]
   end
-  
+
+  def campaigns
+    campaigns = []
+    results = DB.exec("SELECT provinces.* FROM knights
+              JOIN campaigns on (knights.id = campaigns.knight_id)
+              JOIN provinces on (campaigns.province_id = provinces.id)
+              WHERE knights.id = #{id};")
+    results.each do |result|
+      attributes = {
+        :id => result['id'].to_i,
+        :name => result['name'],
+        :type => result['type']
+      }
+      campaigns << Province.new(attributes)
+    end
+    campaigns
+  end
+
   def save
     result = DB.exec("INSERT INTO knights (name) VALUES ('#{name}') RETURNING id;")
     @id = result.first['id'].to_i
   end
-  
+
   def remove
     DB.exec("DELETE FROM knights WHERE id = #{id}")
   end
-  
+
   def == arg
     self.name == arg.name && self.id == arg.id
   end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -5,17 +5,17 @@ describe 'Knight' do
     new_knight = Knight.new({:name => 'Sir Lancelot'})
     expect(new_knight).to be_an_instance_of Knight
   end
-  
+
   it 'lets you read the name' do
     new_knight = Knight.new({:name => 'Sir Lancelot'})
     expect(new_knight.name).to eq 'Sir Lancelot'
   end
-  
+
   describe '.all' do
     it 'starts as an empty array' do
       expect(Knight.all).to eq []
     end
-    
+
     it 'returns all knights' do
       new_knight1 = Knight.new({:name => 'Sir Heckingbottom'})
       new_knight2 = Knight.new({:name => 'Sir Lancelot'})
@@ -24,7 +24,7 @@ describe 'Knight' do
       expect(Knight.all).to eq [new_knight1, new_knight2]
     end
   end
-  
+
   describe '.find' do
     it 'returns a knight when given an id' do
       new_knight = Knight.new({:name => 'Sir Heckingbottom'})
@@ -32,21 +32,21 @@ describe 'Knight' do
       expect(Knight.find(new_knight.id)).to eq new_knight
     end
   end
-  
+
   describe 'save' do
     it 'saves a knight' do
       new_knight = Knight.new({:name => 'Sir Heckingbottom'})
       new_knight.save
       expect(Knight.all).to eq [new_knight]
     end
-    
+
     it 'adds an id' do
       new_knight = Knight.new({:name => 'Sir Heckingbottom'})
       new_knight.save
       expect(Knight.all[0].id).to eq new_knight.id
     end
   end
-  
+
   describe 'remove' do
     it 'deletes a knight' do
       new_knight = Knight.new({:name => 'Sir Reginald Clutterbuck'})
@@ -54,7 +54,7 @@ describe 'Knight' do
       new_knight.remove
       expect(Knight.all).to eq []
     end
-    
+
     it 'only deletes the knight remove is called on' do
       new_knight1 = Knight.new({:name => 'Sir Reginald Clutterbuck'})
       new_knight1.save
@@ -62,6 +62,38 @@ describe 'Knight' do
       new_knight2.save
       new_knight1.remove
       expect(Knight.all).to eq [new_knight2]
+    end
+  end
+
+  describe 'campaigns' do
+    it 'shows which provinces a knight has been sent to' do
+      new_knight = Knight.new({:name => 'Sir Reginald Clutterbuck'})
+      new_knight.save
+      new_province1 = Province.new({:name => 'Castle Blackrock'})
+      new_province1.save
+      new_province2 = Province.new({:name => 'Frozen Fjord'})
+      new_province2.save
+      new_campaign1 = Campaign.new({:knight_id => new_knight.id, :province_id => new_province1.id})
+      new_campaign1.save
+      new_campaign2 = Campaign.new({:knight_id => new_knight.id, :province_id => new_province2.id})
+      new_campaign2.save
+      expect(new_knight.campaigns).to eq [new_province1, new_province2]
+    end
+
+    it 'only shows provinces for what it is called on' do
+      new_knight1 = Knight.new({:name => 'Sir Reginald Clutterbuck'})
+      new_knight1.save
+      new_knight2 = Knight.new({:name => 'Sir Heckingbottom'})
+      new_knight2.save
+      new_province1 = Province.new({:name => 'Castle Blackrock'})
+      new_province1.save
+      new_province2 = Province.new({:name => 'Frozen Fjord'})
+      new_province2.save
+      new_campaign1 = Campaign.new({:knight_id => new_knight1.id, :province_id => new_province1.id})
+      new_campaign1.save
+      new_campaign2 = Campaign.new({:knight_id => new_knight2.id, :province_id => new_province2.id})
+      new_campaign2.save
+      expect(new_knight2.campaigns).to eq [new_province2]
     end
   end
 end
